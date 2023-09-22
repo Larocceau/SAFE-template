3b737aebe50ebd60e77c1f9b96afaaef4cd6ca16
Joost <joost@compositional-it.com>
2023-09-22 14:27:33 +0100

    Add counter and 404 page

--- Content/default/src/Client/Index.fs
+++ Content/default/src/Client/Index.fs
@@ -1,43 +1,70 @@
 module Index
 
-open System
 open Elmish
-open Fable.Remoting.Client
-open Shared
+open Feliz.Router
 
-type Model = {
-    TodoModel: TodoList.Model
-    }
+[<RequireQualifiedAccess>]
+type Url =
+    | TodoList
+    | Counter
+    | NotFound
 
 type Msg =
     | TodoListMsg of TodoList.Msg
+    | CounterMsg of Counter.Msg
+    | UrlChanged of Url
 
-type Url =
-    | TodoList
-    | TodoDetail of Guid
+type Page =
+    | TodoList of TodoList.Model
+    | Counter of Counter.Model
     | NotFound
+type Model = {
+    CurrentPage: Page
+    CurrentUrl: Url
+    }
 
 let parseUrl =
     function
     | [ "todo" ] -> Url.TodoList
-    | [ "todo";  Feliz.Router.Route.Guid item ] -> Url.TodoDetail item
+    | [ "counter" ] -> Url.Counter
     | _ -> Url.NotFound
 
-let init () : Model * Cmd<Msg> =
-    let todoModel, cmd = TodoList.init ()
-    let model = { TodoModel =  todoModel }
 
-    model, cmd |> Cmd.map TodoListMsg
+let initFromUrl url =
+    match url with
+    | Url.TodoList ->
+        let todoModel, command = TodoList.init ()
+        let model = {CurrentPage = TodoList todoModel; CurrentUrl = url }
+        model, command |> Cmd.map TodoListMsg
+    | Url.Counter ->
+        let counterModel, command = Counter.init ()
+        let model = {CurrentPage = Counter counterModel; CurrentUrl = url }
+        model, command |> Cmd.map CounterMsg
+    | Url.NotFound ->
+        let model = { CurrentPage = NotFound; CurrentUrl = url }
+        model, Cmd.none
+
+let init () : Model * Cmd<Msg> =
+    (Router.currentPath())
+    |> parseUrl
+    |> initFromUrl
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
-    match msg with
-    | TodoListMsg todoMsg ->
-        let newTodoModel, msg = TodoList.update todoMsg model.TodoModel
-        let newModel = {model with TodoModel = newTodoModel }
+    match model.CurrentPage, msg with
+    | TodoList todo, TodoListMsg todoMsg ->
+        let newTodoModel, msg = TodoList.update todoMsg todo
+        let newModel = {model with CurrentPage = TodoList newTodoModel }
         newModel, msg |> Cmd.map TodoListMsg
+    | Counter counter, CounterMsg counterMsg ->
+        let newCounterModel, msg = Counter.update counterMsg counter
+        let newModel = {model with CurrentPage = Counter newCounterModel }
+        newModel, msg |> Cmd.map CounterMsg
+    | _, UrlChanged url ->
+        initFromUrl url
 
 open Feliz
 open Feliz.Bulma
+
 
 let navBrand =
     Bulma.navbarBrand.div [
@@ -52,33 +79,45 @@ let navBrand =
             ]
         ]
     ]
+let content page dispatch =
+    match page with
+    | TodoList todoModel -> TodoList.view todoModel (TodoListMsg>>dispatch)
+    | Counter counterModel -> Counter.View counterModel (CounterMsg>>dispatch)
+    | NotFound ->
+        Bulma.box "page not found"
+
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Bulma.hero [
-        hero.isFullHeight
-        color.isPrimary
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
-            style.backgroundPosition "no-repeat center center fixed"
-        ]
-        prop.children [
-            Bulma.heroHead [
-                Bulma.navbar [
-                    Bulma.container [ navBrand ]
+    React.router[
+        router.onUrlChanged (parseUrl>>UrlChanged>>dispatch)
+        router.children[
+            Bulma.hero [
+                hero.isFullHeight
+                color.isPrimary
+                prop.style [
+                    style.backgroundSize "cover"
+                    style.backgroundImageUrl "https://unsplash.it/1200/900?random"
+                    style.backgroundPosition "no-repeat center center fixed"
                 ]
-            ]
-            Bulma.heroBody [
-                Bulma.container [
-                    Bulma.column [
-                        column.is6
-                        column.isOffset3
-                        prop.children [
-                            Bulma.title [
-                                text.hasTextCentered
-                                prop.text "SAFE.App"
+                prop.children [
+                    Bulma.heroHead [
+                        Bulma.navbar [
+                            Bulma.container [ navBrand ]
+                        ]
+                    ]
+                    Bulma.heroBody [
+                        Bulma.container [
+                            Bulma.column [
+                                column.is6
+                                column.isOffset3
+                                prop.children [
+                                    Bulma.title [
+                                        text.hasTextCentered
+                                        prop.text "SAFE.App"
+                                    ]
+                                    content model.CurrentPage dispatch
+                                ]
                             ]
-                            TodoList.view model.TodoModel (TodoListMsg>>dispatch)
                         ]
                     ]
                 ]
